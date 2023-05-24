b59b7cae70da371b42c21859799ef4fe9705e138
albuquerque-lucas <lucaslpra@gmail.com>
2023-05-24 11:06:27 -0300

    Continuando testes, porém com erro no teste do hook fetch

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, renderHook, screen, waitFor, within } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import DataProvider from '../context/DataProvider';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
 import mockFetch from '../../cypress/mocks/fetch';
-import { text } from '@fortawesome/fontawesome-svg-core';
+import testData from '../../cypress/mocks/testData';
+import useFetch from '../hooks/useFetch';
+import Table from '../components/Table';
 
 describe('Testes RTL da aplicação', () => {
   beforeEach(()=> {
@@ -85,7 +87,6 @@ describe('Testes RTL da aplicação', () => {
     const filterValueInput = screen.getByTestId('value-filter');
     const buttonFilter = screen.getByTestId('button-filter');
 
-
     userEvent.selectOptions(comparisonSelect, 'maior que');
     userEvent.selectOptions(columnSelect, 'population');
     userEvent.type(filterValueInput, '1000000');
@@ -97,22 +98,71 @@ describe('Testes RTL da aplicação', () => {
     userEvent.type(filterValueInput, '500');
     userEvent.click(buttonFilter);
 
-
-
-  userEvent.selectOptions(columnSelect, 'rotation_period');
-  userEvent.selectOptions(comparisonSelect, 'igual a');
-  userEvent.clear(filterValueInput);
-  userEvent.type(filterValueInput, '23');
-  userEvent.click(buttonFilter);
-
-
-
+    userEvent.selectOptions(columnSelect, 'rotation_period');
+    userEvent.selectOptions(comparisonSelect, 'igual a');
+    userEvent.clear(filterValueInput);
+    userEvent.type(filterValueInput, '23');
+    userEvent.click(buttonFilter);
 
     const deleteButtons = screen.getAllByRole('button', { name: 'X' })
     expect(deleteButtons).toHaveLength(3);
     userEvent.click(removeAllFiltersBtn);
     const updatedDeleteButtons = screen.queryAllByRole('button', { name: 'X' });
     expect(updatedDeleteButtons).toHaveLength(0);
+  })
+
+  test('define o erro corretamente ao fazer uma requisição com erro', async () => {
+    const mockError = 'Erro ao fazer a requisição';
+    const mockFetch = jest.fn().mockResolvedValueOnce({
+      ok: false,
+      json: async () => ({ message: mockError }),
+    });
+    global.fetch = mockFetch;
+  
+    let result;
+
+    act(() => {
+      const { result: hookResult } = renderHook(() => useFetch('https://swapi.dev/api/planets'));
+      result = hookResult;
+    });
+  
+    expect(result.current.data).toEqual([]);
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.error).toBe(null);
+  
+    await waitFor(() => {
+      return !result.current.isLoading;
+    });
+  
+    act(() => {
+      expect(result.current.isLoading).toBe(false);
+      expect(result.current.data).toEqual([]);
+      expect(result.current.error).toBe(mockError);
+    });
+  
+    expect(mockFetch).toHaveBeenCalledWith('https://swapi.dev/api/planets');
+  });
+
+  test('Testa a função handleFilterOrder', () => {
+    render(
+      <DataProvider>
+        <App/>
+      </DataProvider>
+    )
+
+    const columnSort = screen.getByTestId('column-sort');
+    userEvent.selectOptions(columnSort, 'rotation_period');
+
+    const ascRadioBtn = screen.getByText(/ascendente/i);
+    const radioBtn = within(ascRadioBtn).getByRole('radio');
+    userEvent.click(radioBtn);
+
+    const sortBtn = screen.getByRole('button', {
+      name: /ordenar/i
+    });
+    userEvent.click(sortBtn);
+
+    const planetNames = screen.getAllByTestId('planet-name');
   })
 });
 
